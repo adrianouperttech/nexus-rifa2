f862532af919e105adf6d01ac023fafe14df7077
adriano.uperttech <adriano.uperttech@gmail.com>
2026-04-06 19:55:03 +0000

    chore: Remove dist from git history

--- dist/routes/publicRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const publicController_1 = require("../controllers/publicController");
-const router = (0, express_1.Router)();
-router.get('/raffles/:slug', publicController_1.getRaffleBySlug);
-router.post('/raffles/:slug/reserve', publicController_1.reserveNumbers);
-exports.default = router;
